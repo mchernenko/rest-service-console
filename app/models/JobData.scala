242046f535bef51195aeda13f8dbf14fa83b5b1f
mchernenko <chernenko.maksym@gmail.com>
2016-03-23 18:22:10 +0200

    JobId type changed to Int

--- app/models/JobData.scala
+++ app/models/JobData.scala
@@ -6,18 +6,18 @@ import play.api.libs.functional.syntax._
 
 package object models {
 
-  case class JobData(val jobId: String, val pageCount: Int, val printerName: String) {
+  case class JobData(val jobId: Int, val pageCount: Int, val printerName: String) {
     override def toString = Json.toJson(this).toString()
   }
 
   implicit val jobDataWrites: Writes[JobData] = (
-      (JsPath \ "jobId").write[String] and
+      (JsPath \ "jobId").write[Int] and
       (JsPath \ "pageCount").write[Int] and
       (JsPath \ "printerName").write[String]
     )(unlift(JobData.unapply))
 
   implicit val placeReads: Reads[JobData] = (
-    (JsPath \ "jobId").read[String] and
+    (JsPath \ "jobId").read[Int] and
     (JsPath \ "pageCount").read[Int] and
     (JsPath \ "printerName").read[String]
     )(JobData.apply _)
