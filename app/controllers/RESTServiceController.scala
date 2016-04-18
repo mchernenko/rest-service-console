eba549afb39041af412b3c70327d035813e8a8c4
mchernenko <chernenko.maksym@gmail.com>
2016-04-18 12:38:23 +0300

    Job data submission API implemented

--- app/controllers/RESTServiceController.scala
+++ app/controllers/RESTServiceController.scala
@@ -1,20 +1,94 @@
 package controllers
 
 import models._
-import play.api.libs.json.Json
-import play.api.mvc.{Action, BodyParsers, Controller}
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc._
 
 object RESTServiceController extends Controller{
+
+  private val ACCESS_TOKEN = "jeoqRQl/yLSudhewBXF/e1PkJBpfxqv89LjlL/9DJAyFJq07FmtjVCMa84EBu0/V0T7ZYtcKE21h4CxKUIaVelOxFsDJUa8" +
+    "kPr8lk1gyRrgGSRM+nTXpe+5s3KrSU54+17GFAmraDJa+rSjkpLWQ+b+rUaQMSom0qMVl6ZG9mlsSzctiw7cKNghY988TiARTpSF3ckpp4AzjB2" +
+    "dvYrIG2SVa2lKQsvSvtt6jMCOYbF+WthQF7GJBnYMWgdyJmuPS6KC9M1fg1EOJjOeiZmIc1SouVxGuqUJ19CSKd6u5Ln27XQGGPOGHqjtaP5Dra" +
+    "WxFY4MHM97Cxz5rOayit1NUz9RBnk8brcENkIHYIryhzV3NQUxHsGJ5F/8JUu7a1tbv"
+
+  private val ACCESS_TOKEN_HEADER = "access-token";
+
+  private val USERNAME = "qwerty"
+  private val PASSWORD = "asdfgh"
+
+  private val BAD_REQUEST_MESSAGE = "Invalid data"
+  private val INCORRECT_CREDENTIALS_MESSAGE = "User not found"
+  private val AUTHENTICATION_REQUIRED = "Authentication required"
+  private val OK_MESSAGE = "OK"
 
   def submitJobData = Action(BodyParsers.parse.json) { request =>
     request.body.validate[JobData].fold(
       errors => {
-        BadRequest("Invalid data")
+        BadRequest(BAD_REQUEST_MESSAGE)
       },
       jobData => {
         WebSocketController.sendMessageToAll(jobData.toString)
-        Ok("OK")
+        Ok(OK_MESSAGE)
       }
     )
+  }
+
+  def authenticate = Action(BodyParsers.parse.json) { implicit request =>
+    request.body.validate[AuthenticateData].fold(
+      errors => {
+        BadRequest(BAD_REQUEST_MESSAGE)
+      },
+      authenticateData => {
+        WebSocketController.sendMessageToAll("Authentication request received")
+        WebSocketController.sendMessageToAll(authenticateData.toString)
+        if(authenticateData.username == USERNAME && authenticateData.password == PASSWORD) {
+          val json: JsValue = Json.obj(ACCESS_TOKEN_HEADER -> ACCESS_TOKEN)
+          WebSocketController.sendMessageToAll("Sending access token")
+          WebSocketController.sendMessageToAll(json.toString)
+          Ok(json)
+        }
+        else {
+          Forbidden(INCORRECT_CREDENTIALS_MESSAGE)
+        }
+      }
+    )
+  }
+
+  def startJob = Action(BodyParsers.parse.json) { implicit request =>
+    request.body.validate[StartJobData].fold(
+      errors => {
+        BadRequest(BAD_REQUEST_MESSAGE)
+      },
+      startJobData => {
+        WebSocketController.sendMessageToAll("Start job request received")
+        WebSocketController.sendMessageToAll(startJobData.toString)
+        checkAuthentication
+      }
+    )
+  }
+
+  def endJob = Action(BodyParsers.parse.json) { implicit request =>
+    request.body.validate[EndJobData].fold(
+      errors => {
+        BadRequest(BAD_REQUEST_MESSAGE)
+      },
+      endJobData => {
+        WebSocketController.sendMessageToAll("End job request received")
+        WebSocketController.sendMessageToAll(endJobData.toString)
+        checkAuthentication
+      }
+    )
+  }
+
+  private def checkAuthentication(implicit request: Request[_]): Result = {
+    request.headers.get(ACCESS_TOKEN_HEADER) match {
+      case Some(accessToken) => {
+        if(accessToken == ACCESS_TOKEN)
+          Ok(OK_MESSAGE)
+        else
+          Forbidden(INCORRECT_CREDENTIALS_MESSAGE)
+      }
+      case None => Forbidden(AUTHENTICATION_REQUIRED);
+    }
   }
 }
--- app/models/JobData.scala
+++ app/models/JobData.scala
@@ -6,21 +6,70 @@ import play.api.libs.functional.syntax._
 
 package object models {
 
+  // JobData
   case class JobData(val jobId: Int, val pageCount: Int, val printerName: String) {
     override def toString = Json.toJson(this).toString()
   }
 
   implicit val jobDataWrites: Writes[JobData] = (
-      (JsPath \ "jobId").write[Int] and
-      (JsPath \ "pageCount").write[Int] and
-      (JsPath \ "printerName").write[String]
+    (JsPath \ "jobId").write[Int] and
+    (JsPath \ "pageCount").write[Int] and
+    (JsPath \ "printerName").write[String]
     )(unlift(JobData.unapply))
 
-  implicit val placeReads: Reads[JobData] = (
+  implicit val jobDataReads: Reads[JobData] = (
     (JsPath \ "jobId").read[Int] and
     (JsPath \ "pageCount").read[Int] and
     (JsPath \ "printerName").read[String]
     )(JobData.apply _)
 
+  // AuthenticateData
+  case class AuthenticateData(val username: String, val password: String) {
+    override def toString = Json.toJson(this).toString()
+  }
+
+  implicit val authenticateDataWrites: Writes[AuthenticateData] = (
+    (JsPath \ "UserName").write[String] and
+    (JsPath \ "Password").write[String]
+    )(unlift(AuthenticateData.unapply))
+
+  implicit val authenticateDataReads: Reads[AuthenticateData] = (
+    (JsPath \ "UserName").read[String] and
+    (JsPath \ "Password").read[String]
+    )(AuthenticateData.apply _)
+
+  // StartJobData
+  case class StartJobData(val ipAddress: String, val userName: String, val jobData: String) {
+    override def toString = Json.toJson(this).toString()
+  }
+
+  implicit val startJobDataWrites: Writes[StartJobData] = (
+    (JsPath \ "IPAddress").write[String] and
+    (JsPath \ "UserName").write[String] and
+    (JsPath \ "Jobdata").write[String]
+    )(unlift(StartJobData.unapply))
+
+  implicit val startJobDataReads: Reads[StartJobData] = (
+    (JsPath \ "IPAddress").read[String] and
+    (JsPath \ "UserName").read[String] and
+    (JsPath \ "Jobdata").read[String]
+    )(StartJobData.apply _)
+
+  // EndJobData
+  case class EndJobData(val ipAddress: String, val userName: String, val result: String) {
+    override def toString = Json.toJson(this).toString()
+  }
+
+  implicit val endJobDataWrites: Writes[EndJobData] = (
+    (JsPath \ "IPAddress").write[String] and
+    (JsPath \ "UserName").write[String] and
+    (JsPath \ "Result").write[String]
+    )(unlift(EndJobData.unapply))
+
+  implicit val endJobDataReads: Reads[EndJobData] = (
+    (JsPath \ "IPAddress").read[String] and
+    (JsPath \ "UserName").read[String] and
+    (JsPath \ "Result").read[String]
+    )(EndJobData.apply _)
 
 }
